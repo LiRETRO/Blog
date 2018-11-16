d3494c731c402cb0dbd9127f3b5d3af4e671be66
LiRETRO <hash@meloli.net>
2018-11-16 16:32:53 +0800

    分组件开发，过渡动画

--- src/assets/js/router/routerPath.js
+++ src/assets/js/router/routerPath.js
@@ -1,4 +1,12 @@
+import { isAbsolute } from 'path';
+
 const Layout = () => import ('@/components/Layout/Layout.vue')
+const about = () => import ('@/views/about.vue')
+const experience = () => import ('@/views/experience.vue')
+const education = () => import ('@/views/education.vue')
+const skills = () => import ('@/views/skills.vue')
+const interests = () => import ('@/views/interests.vue')
+const awards = () => import ('@/views/awards.vue')
 
 export default [
   {
@@ -8,7 +16,55 @@ export default [
     meta: {
       title: '主页'
     },
-    childrens: [
+    children: [
+      {
+        path: '/about',
+        name: 'about',
+        component: about,
+        meta: {
+          title: '关于'
+        }
+      },
+      {
+        path: '/experience',
+        name: 'experience',
+        component: experience,
+        meta: {
+          title: '经验'
+        }
+      },
+      {
+        path: '/education',
+        name: 'education',
+        component: education,
+        meta: {
+          title: 'education'
+        }
+      },
+      {
+        path: '/skills',
+        name: 'skills',
+        component: skills,
+        meta: {
+          title: 'skills'
+        }
+      },
+      {
+        path: '/interests',
+        name: 'interests',
+        component: interests,
+        meta: {
+          title: 'interests'
+        }
+      },
+      {
+        path: '/awards',
+        name: 'awards',
+        component: awards,
+        meta: {
+          title: 'awards'
+        }
+      },
     ]
   }
 ]
--- src/components/Layout/Layout.vue
+++ src/components/Layout/Layout.vue
@@ -1,39 +1,44 @@
 <template>
   <div class="main" id="page-top">
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
-      <a class="navbar-brand js-scroll-trigger" href="#page-top">
+      <router-link class="navbar-brand js-scroll-trigger" to="/">
         <span class="d-block d-lg-none">Clarence Taylor</span>
         <span class="d-none d-lg-block">
           <img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="../../../static/images/head.png" alt="">
         </span>
-      </a>
+      </router-link>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="#about">About</a>
+            <router-link class="nav-link js-scroll-trigger" to="/about">About</router-link>
           </li>
           <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="#experience">Experience</a>
+            <router-link class="nav-link js-scroll-trigger" to="/experience">Experience</router-link>
           </li>
           <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="#education">Education</a>
+            <router-link class="nav-link js-scroll-trigger" to="/education">Education</router-link>
           </li>
           <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="#skills">Skills</a>
+            <router-link class="nav-link js-scroll-trigger" to="/skills">Skills</router-link>
           </li>
           <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="#interests">Interests</a>
+            <router-link class="nav-link js-scroll-trigger" to="/interests">Interests</router-link>
           </li>
           <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="#awards">Awards</a>
+            <router-link class="nav-link js-scroll-trigger" to="/awards">Awards</router-link>
           </li>
         </ul>
       </div>
     </nav>
-    <content-view></content-view>
+    <div class="container-fluid p-0">
+      <transition :name="transitionName" mode="out-in">
+        <router-view class="child-view" :v-show="isIndex"/>
+        <img class="child-view" :v-show="isIndex" src="../../../static/images/bg.png" style="width: 100%; height: 100%;"/>
+      </transition>
+    </div>
   </div>
 </template>
 
@@ -61,22 +66,43 @@ $(function () {
     target: '#sideNav'
   });
 })
-import content from '@/views/content.vue'
 export default {
   data () {
     return {
-
+      transitionName: 'slide-top',
+      menu: [
+        'about', 'experience', 'education', 'skills', 'interests', 'awards'
+      ],
+      isIndex: true
     }
   },
   methods: {
     init () {
     }
   },
+  watch: {
+    '$route' (to, from) {
+      const toDepth = this.menu.findIndex(item => item === to.name)
+      const fromDepth = this.menu.findIndex(item => item === from.name)
+      this.transitionName = toDepth < fromDepth ? 'slide-top' : 'slide-down'
+      this.isIndex = to.path != '/' ? false : true
+    }
+  },
   mounted () {
     this.init()
-  },
-  components: {
-    'content-view': content
   }
 }
 </script>
+<style lang="less">
+.child-view {
+  transition: all .3s cubic-bezier(.68, -1.5, .27, 2);
+}
+.slide-top-enter, .slide-down-leave-active  {
+  transform: translateY(-75px);
+  opacity: 0;
+}
+.slide-top-leave-active, .slide-down-enter  {
+  transform: translateY(75px);
+  opacity: 0;
+}
+</style>
