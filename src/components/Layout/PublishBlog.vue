d0127f06eb1d3f2886a2f1c50c43890a920d0eab
LiRETRO <hash@meloli.net>
2019-03-03 20:27:05 +0800

    loading窗口高度继承body

--- src/components/Layout/PublishBlog.vue
+++ src/components/Layout/PublishBlog.vue
@@ -41,7 +41,7 @@ export default {
                 if (data.resultCode === 'success') {
                     alert(data.resultMessage)
                 }
-            })
+            });
         }
     },
     mounted () {
