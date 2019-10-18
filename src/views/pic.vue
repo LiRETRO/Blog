b37503532758bbdd5b4f963ab4d98de93f8b7698
LiRETRO <hash@meloli.net>
2019-10-18 23:38:00 +0800

    '修改首页图片大小'

--- src/views/pic.vue
+++ src/views/pic.vue
@@ -1,4 +1,4 @@
 <template>
-  <!-- <div style="background: url('../../static/images/bg.jpg'); background-size: 100% 100%; width: 100%; height: 100%; position: absolute;"></div> -->
-    <img src="/static/images/bg.jpg" style="position: absolute; left: 0; top: 51px; width: 100%; height: 100%;"/>
+  <!-- <div style="background: url('/static/images/bg.jpg'); background-size: 100% 100%; width: 100%; height: 100%; position: absolute;"></div> -->
+    <img src="/static/images/bg.jpg" style="position: absolute; left: 0; top: 51px; width: 100%; height: 90%; z-index: -1;"/>
 </template>
